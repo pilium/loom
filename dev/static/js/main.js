1c916474e7b93dac67a118fae5011544c0366c60
pilium <igorpilium@gmail.com>
2017-05-16 22:05:06 +0300

    Готовая верстка сайта
    необходимо доделать mobile menu

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,13 +1,38 @@
-
 // Начинать писать отсюда!!!!
-$(document).ready(function(){
+$( document ).ready( function () {
+
+  $( '.js-banner-slider' ).slick( {
+    dots: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '.banner-slider__prev',
+    nextArrow: '.banner-slider__next'
+  } );
+  $( '.js-opinions-slider' ).slick( {
+    dots: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false
+  } );
+  $( '.nav-mobile' ).click( function () {
+    $( this ).toggleClass( 'active' );
+    $( '.main-nav__list' ).toggleClass( 'mobile-show' );
+  } );
+  $('.popup-link').magnificPopup({
+    type: 'inline',
     
-    $('.js-banner-slider').slick({
-        dots: false,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '.banner-slider__prev',
-        nextArrow: '.banner-slider__next'
-    });
-});
+  })
+} );
+$( window ).scroll( function () {
+  if ( $( this ).scrollTop() > 10 ) {
+    $( '.header-top__content' ).addClass( "sticky" );
+  } else {
+    $( '.header-top__content' ).removeClass( "sticky" );
+  }
+} );
